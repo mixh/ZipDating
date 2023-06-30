adfcdd0e2fc649a8c55b4c7e06b0b4c87c8c078d
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-06-30 12:20:42 -0400

    atlas

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,6 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
 export const mongoConfig = {
-  serverUrl: "mongodb://127.0.0.1:27017/",
+  serverUrl:
+    "mongodb+srv://user1:WGYevH0q4jYkBD5f@cluster0.kjsmrot.mongodb.net/?retryWrites=true&w=majority",
   database: "Final_Project_MJ_DEV",
 };

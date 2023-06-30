f0311b6f6f4d5a3435d1350d6f366ee93e166292
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-06-30 13:11:07 -0400

    heroku deploy

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,8 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const mongoConfig = {
-  serverUrl:
-    "mongodb+srv://user1:WGYevH0q4jYkBD5f@cluster0.kjsmrot.mongodb.net/?retryWrites=true&w=majority",
+  serverUrl: `mongodb+srv://user1:${process.env.PASSWORD}@cluster0.kjsmrot.mongodb.net/?retryWrites=true&w=majority`,
   database: "Final_Project_MJ_DEV",
 };
